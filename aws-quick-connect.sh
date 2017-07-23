cba424aaecf2083a96ea16bcce9d03a912d8daab
Michael Pearce <michael.pearce@peak.bi>
2017-07-23 23:58:14 +0100

    Added getopts functions
    * added getopts functions for cleaner parameter input
    * added username option - default is ec2-user

--- aws-quick-connect.sh
+++ aws-quick-connect.sh
@@ -1,37 +1,70 @@
 #!/bin/bash
+#serverlist=".ServerList"
 serverlist=".ServerList"
+user="ec2-user"
 
 function init() {
-serveralias=$1
-servername=`getServerName $1`
-keypair="~/.ssh/$2"
-awsprofile=$3
-echo "Connecting to server: $servername"
-echo "Using keypair: $keypair"
-echo "Running as aws profile: $awsprofile"
+
+if [ -z "$serveralias" ]
+then
+  echo "Error! Please provide server alias (-s myserver)"
+else
+  echo "Server alias is: $serveralias"
+fi
+
+if [ -z "$keypair" ]
+then
+  echo "Error! Please provide ssh keypair (-k keypair.pem)"
+else
+  echo "Using keypair: $keypair"
+fi
+
+if [ -z "$awsprofile" ]
+then
+  echo "Error! Please provide aws profile (-p my-aws-profile)"
+else
+  echo "Running as aws profile: $awsprofile"
+fi
+
+servername=`getServerName`
+if [ -z "$servername" ]
+then
+  echo "Error! Can't find servername. Make sure you add it to the template file."
+  exit 1
+else
+  echo "Servername is : $servername"
+  connect
+fi
 
 }
 
 function connect() {
-  IPAddress=`getLastIP $servername` || getNewIP $1 $3
-  echo "Attempting to connect to $IPAddress"
-  ssh -i $keypair ec2-user@$IPAddress || reconnect $servername $2 $3
+
+  IPAddress=`getLastIP $servername`
+  if [ -z "$IPAddress" ]
+  then
+    getNewIP
+  else
+    ssh -i $keypair $user@$IPAddress || reconnect
+  fi
   # check hostname
 }
 
 function reconnect() {
-  getNewIP $servername $3
+  echo "Starting reconnect()"
+
+  getNewIP
   IPAddress=`getLastIP $servername` || echo "can't find IP"
   echo "Attempting to reconnect with $IPAddress"
-  ssh -i $keypair ec2-user@$IPAddress
+  ssh -i $keypair $user@$IPAddress
 }
 
 function getServerName() {
-  cat $serverlist | grep $1 | cut -d ":" -f 2 | cut -d "=" -f 1 | head -1
+  cat $serverlist | grep $serveralias | cut -d ":" -f 2 | cut -d "=" -f 1 | head -1
 }
 
 function getLastIP() {
-  cat $serverlist | grep "$servername" | cut -d "=" -f 2
+  cat $serverlist | grep $servername | cut -d "=" -f 2
 }
 
 function addToIPList() {
@@ -47,11 +80,36 @@ function removeIP() {
 
 function getNewIP() {
   echo Getting new IP...
+
   newIPAddress="`aws ec2 --profile $awsprofile describe-instances --output table \
   --filters "Name=tag:Name,Values=$servername" | grep -m 1 PrivateIpAddress \
   | awk '{ print $4 ;}'`" && echo New IP fetched successfully, new IP is $newIPAddress
   addToIPList $newIPAddress
 }
 
-init $1 $2 $3
-connect $servername $keypair $awsprofile
+while getopts ":s:k:p:u:" opt; do
+  case $opt in
+    s)
+      serveralias=$OPTARG
+      ;;
+    k)
+      keypair=~/.ssh/$OPTARG
+      ;;
+    p)
+      awsprofile=$OPTARG
+      ;;
+    u)
+      user=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+init
